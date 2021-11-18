9f784cdcae56c597bccfa38c9b1f22c2a25e79d0
Hassan BEN ZAID <hassan_benz@hotmail.fr>
2021-11-18 20:46:25 +0100

    commit fin du TP3

--- projets/pizzeria/lib/models/pizza.dart
+++ projets/pizzeria/lib/models/pizza.dart
@@ -1,3 +1,5 @@
+import 'package:pizzeria/models/option_item.dart';
+
 class Pizza {
   final int id;
   final String title;
@@ -5,5 +7,36 @@ class Pizza {
   final String image;
   final double price;
 
+  // La sélection de l'utilisateur
+  int pate = 0;
+  int taille = 1;
+  int sauce = 0;
+
+  // Les options possibles
+  static final List<OptionItem> pates = [
+    OptionItem(0, "Pâte fine"),
+    OptionItem(1, "Pâte épaisse", supplement: 2),
+  ];
+  static final List<OptionItem> tailles = [
+    OptionItem(0, "Small", supplement: -1),
+    OptionItem(1, "Medium"),
+    OptionItem(1, "Large", supplement: 2),
+    OptionItem(1, "Extra large", supplement: 2),
+  ];
+  static final List<OptionItem> sauces = [
+    OptionItem(0, "Base sauce tomate"),
+    OptionItem(1, "Sauce Samourai", supplement: 2),
+  ];
+
   Pizza(this.id, this.title, this.garniture, this.image, this.price);
+
+  double get total {
+    double total = price;   // prix de base
+
+    total += pates[pate].supplement;  //pâte
+    total += tailles[taille].supplement; // Taille
+    total += sauces[sauce].supplement;   // sauce
+
+    return total;
+  }
 }
--- projets/pizzeria/lib/ui/pizza_list.dart
+++ projets/pizzeria/lib/ui/pizza_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pizzeria/models/pizza.dart';
 import 'package:pizzeria/models/pizza_data.dart';
+import 'package:pizzeria/ui/pizza_details.dart';
+import 'package:pizzeria/ui/share/buy_button_widget.dart';
 
 
 class PizzaList extends StatefulWidget {
@@ -23,16 +25,16 @@ class _PizzaListState extends State<PizzaList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Nos Pizzas'),
-      ),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8.0),
-          //itemExtent: ,
-          itemCount: _pizzas.length,
-          itemBuilder: (context, index) {
-            return _buildRow(_pizzas[index]);
-          }));
+        appBar: AppBar(
+          title: Text('Nos Pizzas'),
+        ),
+        body: ListView.builder(
+            padding: const EdgeInsets.all(8.0),
+            //itemExtent: ,
+            itemCount: _pizzas.length,
+            itemBuilder: (context, index) {
+              return _buildRow(_pizzas[index]);
+            }));
   }
 
   _buildRow(Pizza pizza) {
@@ -40,54 +42,75 @@ class _PizzaListState extends State<PizzaList> {
       // color: Theme.of(context).cardColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(
-          bottom: Radius.circular(10.0), top: Radius.circular(2.0)),
+            bottom: Radius.circular(10.0), top: Radius.circular(2.0)),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ListTile(
-            title: Text(pizza.title),
-            subtitle: Text(pizza.garniture),
-            leading: Icon(Icons.local_pizza),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PizzaDetails(pizza),
+                ),
+              );
+            },
+            child: _buildPizzaDetails(pizza),
           ),
-          Image.asset(
-            'assets/images/pizzas/${pizza.image}',
-            height: 120,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.fitWidth,
-          ),
-          /*Text(pizza.garniture), */
-          Container(
-            padding: const EdgeInsets.all(4.0),
-            child: Text(pizza.garniture),
-          ),
-         Row(
-           mainAxisAlignment: MainAxisAlignment.end,
-           children: [
-             ElevatedButton(
-               style: ButtonStyle(
-                 backgroundColor: MaterialStateProperty.all<Color>(Colors.red.shade800)),
-               child: Row(
-                 children: [
-                   Icon(Icons.shopping_cart),
-                   SizedBox(width: 5),
-                   Text('Commander'),
-                 ],
-               ),
-               onPressed: () {
-                 print('Commander une pizza');
-               },
-             ),
-           ],
-         ),
-                               /* ElevatedButton(
-                                  child: Text("Commander"),
-                                  onPressed: () {
-                                    print('Commander une pizza');
-                                  },
-                                ),   */
+          BuyButtonWidget(),
         ],
       ),
     );
+  }
+
+  _buildPizzaDetails(Pizza pizza) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListTile(
+          title: Text(pizza.title),
+          subtitle: Text(pizza.garniture),
+          leading: Icon(Icons.local_pizza),
+        ),
+        Image.asset(
+          'assets/images/pizzas/${pizza.image}',
+          height: 120,
+          width: MediaQuery
+              .of(context)
+              .size
+              .width,
+          fit: BoxFit.fitWidth,
+        ),
+        Container(
+          padding: const EdgeInsets.all(4.0),
+          child: Text(pizza.garniture),
+        ),
+      ],
+    );
+  }
+
+  _buildBuyButton() {
+    return
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.red.shade800)),
+            child: Row(
+              children: [
+                Icon(Icons.shopping_cart),
+                SizedBox(width: 5,),
+                Text("Commander"),
+              ],
+            ),
+            onPressed: () {
+              print('Commander une pizza');
+            },
+          ),
+        ],
+      );
   }
 }
